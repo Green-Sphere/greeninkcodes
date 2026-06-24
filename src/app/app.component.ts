ba7ac9d223f4b2117da5f1e3e210bc28a850176d
Josh <josh@greensphere.tech>
2026-06-24 15:36:04 -0400

    Changed layout and icons, added dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,20 @@ import { HeaderComponent } from './header/header.component';
   standalone: true,
   imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'GREENINK';
   tagline = 'Simplify your QR';
+
+  // In your app.component.ts
+  ngOnInit() {
+    const dark = window.matchMedia('(prefers-color-scheme: dark)');
+    if (dark.matches) {
+      document.body.classList.add('dark-mode');
+    }
+    dark.addEventListener('change', (e) => {
+      document.body.classList.toggle('dark-mode', e.matches);
+    });
+  }
 }
